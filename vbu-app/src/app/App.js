fb105b53602a874de9c69923481579809f0c80f8
asmanov <asmanov.a@gmail.com>
2023-03-12 09:26:39 +0200

    first commit

---
+++ vbu-app/src/app/App.js
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+// import logo from '../logo.svg';
+import './App.css';
+
+function App() {
+  <div>
+    <h1>Калькулятор остановочного пути автомобиля</h1>
+    <div>
+      
+    </div>
+  </div>
+  // const [distance, setDistance] = useState('');
+  // const [avgR, setavgR] = useState('');
+  // const [valueFuel, setvalueFuel] = useState('');
+  // const [cost, setCost] = useState('');
+  // function changedistance(e){
+  //   setDistance(e.target.value);
+  //   console.warn(distance);
+  // }
+  // function changeR(e){
+  //   setavgR(e.target.value);
+  //   console.warn(avgR);
+  //   }
+  // function changeValueFuel(e){
+  //   setvalueFuel(e.target.value);
+  //   console.warn(valueFuel);
+  // }
+  // function calcValue(){
+  //   let distint = parseInt(distance);
+  //   console.warn(distint);
+  //   let rint = parseInt(avgR);
+  //   let fuelint = parseInt(valueFuel);
+  //   let costint = distint / 100 * rint * fuelint;
+  //   console.warn(costint);
+  //   setCost(`${costint}`);   
+  // }
+  return (
+    // <div id='main'>
+    //   <h1>Стоимость поездки</h1>
+    //   <p>Растояние</p>
+    //   <input type='text' value={distance} onChange={changedistance}></input>
+    //   <label>  km</label>
+    //   <p>Средний расход</p>
+    //   <input type='text' value={avgR} onChange={changeR} ></input>
+    //   <label>  L</label>
+    //   <p>Стоимость топлива</p>
+    //   <input type='text' value={valueFuel} onChange={changeValueFuel}></input>
+    //   <label>  Grn</label>
+    //   <br /> <br />
+    //   <button onClick={calcValue}>Расчитать</button>
+    //   <br /> <br />
+    //   <div>{cost}</div>
+    // </div>
+    
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
